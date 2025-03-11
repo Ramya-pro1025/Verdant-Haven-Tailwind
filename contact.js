aff19fee069c0368051bdef319d6ef86b18c0768
Ramya-pro1025 <privatespace346@gmail.com>
2025-03-12 00:32:41 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,13 @@
+//Selectinh Side Navbar,MenuIcon
+
+var sidenav = document.getElementById("sidenav")
+var menuIcon = document.getElementById("menuIcon")
+var closenav = document.getElementById("closenav")
+
+menuIcon.addEventListener("click",function()
+{
+    sidenav.style.right=0
+})
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
